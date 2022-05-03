58a25899b5f98cfafcc12a9f7febcd94b785fe69
Enes T <ahmedenesturan@gmail.com>
2022-05-04 00:13:49 +0300

    Update cloudtrail.tf

--- cloudtrail.tf
+++ cloudtrail.tf
@@ -1,6 +1,6 @@
 data "aws_caller_identity" "current" {}
   
-data "template_file" "cloudtrial_policy" {
+data "template_file" "cloudtrail_policy" {
   template = file("${path.module}/policy.json")
 }
 
@@ -11,12 +11,12 @@ resource "aws_cloudtrail" "foobar" {
   include_global_service_events = false
 }
 
-resource "aws_s3_bucket_policy" "trail_policy" {
-  bucket = aws_s3_bucket.s3devenes.id
-  policy = data.template_file.cloudtrial_policy.rendered
-}
-
 resource "aws_s3_bucket" "s3devenes" {
   bucket        = "devenescloudbucket"
   force_destroy = true
+}
+
+resource "aws_s3_bucket_policy" "trail_policy" {
+  bucket = aws_s3_bucket.s3devenes.id
+  policy = data.template_file.cloudtrail_policy.rendered
 }
