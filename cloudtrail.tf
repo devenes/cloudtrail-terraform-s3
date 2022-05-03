c599a985ab05432990b8e6d93dd9cdd77237baaa
Enes T <ahmedenesturan@gmail.com>
2022-05-04 00:01:30 +0300

    Create cloudtrail.tf

---
+++ cloudtrail.tf
@@ -0,0 +1,18 @@
+data "aws_caller_identity" "current" {}
+  
+data "template_file" "cloudtrial_policy" {
+  template = file("${path.module}/policy.json")
+}
+
+resource "aws_cloudtrail" "foobar" {
+  name                          = "devenescloud"
+  s3_bucket_name                = aws_s3_bucket.s3devenes.id
+  s3_key_prefix                 = "prefix"
+  include_global_service_events = false
+}
+
+resource "aws_s3_bucket" "s3devenes" {
+  bucket        = "devenescloudbucket"
+  force_destroy = true
+  policy        = data.template_file.cloudtrial_policy.rendered
+}
