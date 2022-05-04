70a7644290c5e73c08e5706aee54778843d13dae
Enes <66560757+devenes@users.noreply.github.com>
2022-05-04 17:54:58 +0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,6 @@ output "s3_bucket_name" {
   value = aws_cloudtrail.foobar.s3_bucket_name
 }
 
-output "aws_account_id" {
-  value = data.aws_caller_identity.current.account_id
-}
+# output "aws_account_id" {
+#   value = data.aws_caller_identity.current.account_id
+# }
