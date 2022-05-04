efb3ebb820514c069262a0b2359beac05145365b
Enes <66560757+devenes@users.noreply.github.com>
2022-05-04 18:01:11 +0300

    Update cloudtrail.tf

--- cloudtrail.tf
+++ cloudtrail.tf
@@ -18,5 +18,35 @@ resource "aws_s3_bucket" "s3devenes" {
 
 resource "aws_s3_bucket_policy" "trail_policy" {
   bucket = aws_s3_bucket.s3devenes.id
-  policy = data.template_file.cloudtrail_policy.rendered
+  # policy = data.template_file.cloudtrail_policy.rendered
+  policy = <<POLICY
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "AWSCloudTrailAclCheck",
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "cloudtrail.amazonaws.com"
+        },
+        "Action": "s3:GetBucketAcl",
+        "Resource": "arn:aws:s3:::devenescloudbucket"
+      },
+      {
+        "Sid": "AWSCloudTrailWrite",
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "cloudtrail.amazonaws.com"
+        },
+        "Action": "s3:PutObject",
+        "Resource": "arn:aws:s3:::devenescloudbucket/prefix/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
+        "Condition": {
+          "StringEquals": {
+            "s3:x-amz-acl": "bucket-owner-full-control"
+          }
+        }
+      }
+    ]
+  }
+POLICY
 }
