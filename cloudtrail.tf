82895e25a29af44f3c43daae3b25304d5e9a326d
Enes <66560757+devenes@users.noreply.github.com>
2022-05-04 17:33:57 +0300

    Update cloudtrail.tf

--- cloudtrail.tf
+++ cloudtrail.tf
@@ -1,11 +1,11 @@
 data "aws_caller_identity" "current" {}
 
 data "template_file" "cloudtrail_policy" {
-  template = file("${path.module}/policy.json")
+  template = file("${path.module}/s3policy.json")
 }
 
 resource "aws_cloudtrail" "foobar" {
-  name                          = "devenescloud"
+  name                          = "devenescloudtrail"
   s3_bucket_name                = aws_s3_bucket.s3devenes.id
   s3_key_prefix                 = "prefix"
   include_global_service_events = false
@@ -19,35 +19,4 @@ resource "aws_s3_bucket" "s3devenes" {
 resource "aws_s3_bucket_policy" "trail_policy" {
   bucket = aws_s3_bucket.s3devenes.id
   policy = data.template_file.cloudtrail_policy.rendered
-
-  #   policy = <<POLICY
-  # {
-  #   "Version": "2012-10-17",
-  #   "Statement": [
-  #     {
-  #       "Sid": "AWSCloudTrailAclCheck",
-  #       "Effect": "Allow",
-  #       "Principal": {
-  #         "Service": "cloudtrail.amazonaws.com"
-  #       },
-  #       "Action": "s3:GetBucketAcl",
-  #       "Resource": "arn:aws:s3:::devenescloudbucket"
-  #     },
-  #     {
-  #       "Sid": "AWSCloudTrailWrite",
-  #       "Effect": "Allow",
-  #       "Principal": {
-  #         "Service": "cloudtrail.amazonaws.com"
-  #       },
-  #       "Action": "s3:PutObject",
-  #       "Resource": "arn:aws:s3:::devenescloudbucket/prefix/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
-  #       "Condition": {
-  #         "StringEquals": {
-  #           "s3:x-amz-acl": "bucket-owner-full-control"
-  #         }
-  #       }
-  #     }
-  #   ]
-  # }
-  # POLICY
 }
